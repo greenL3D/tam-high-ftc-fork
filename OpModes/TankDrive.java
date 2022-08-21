568d6016d930a7fa61cda4f4636c13b2e56280b1
Nicholas Sullivan-Friedman <rpgcubed@gmail.com>
2022-08-21 11:42:26 -0700

    style: Java tabs (4 spaces)

--- OpModes/TankDrive.java
+++ OpModes/TankDrive.java
@@ -7,21 +7,20 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class TankDrive extends LinearOpMode {
-  private DcMotor left_front, right_front;
-  private DcMotor left_back, right_back;
+    private DcMotor left_front, right_front;
+    private DcMotor left_back, right_back;
 
-  @Override
-  public void runOpMode() {
-    leftFront = hardwareMap.get(DcMotor.class, "left_front");
-    rightFront = hardwareMap.get(DcMotor.class, "right_front");
-    leftBack = hardwareMap.get(DcMotor.class, "left_back");
-    rightBack = hardwareMap.get(DcMotor.class, "right_back");
+    @Override
+    public void runOpMode() {
+        leftFront = hardwareMap.get(DcMotor.class, "left_front");
+        rightFront = hardwareMap.get(DcMotor.class, "right_front");
+        leftBack = hardwareMap.get(DcMotor.class, "left_back");
+        rightBack = hardwareMap.get(DcMotor.class, "right_back");
 
-    waitForStart();
+        waitForStart();
 
-    while (opModeIsActive()) {
-    
-    
+        while (opModeIsActive()) {
+
+        }
     }
-  }
 }
