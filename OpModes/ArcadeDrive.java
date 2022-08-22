3bcb23b0de3d7ab71d4da378ea12b3679b484aa7
carlank <rpgcubed@gmail.com>
2022-08-22 10:47:41 -0700

    feat: add ArcadeDrive OpMode

---
+++ OpModes/ArcadeDrive.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name="Template: Arcade Drive", group="Linear Opmode")
+public class ArcadeDrive extends LinearOpMode {
+    // Declare the hardware variables
+    private DcMotor left_front, right_front;
+    private DcMotor left_back, right_back;
+
+    @Override
+    public void runOpMode() {
+        // Initialize the hardware variables. Note that the strings used here as
+        // parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        leftFront = hardwareMap.get(DcMotor.class, "left_front");
+        rightFront = hardwareMap.get(DcMotor.class, "right_front");
+        leftBack = hardwareMap.get(DcMotor.class, "left_back");
+        rightBack = hardwareMap.get(DcMotor.class, "right_back");
+
+        // One of the pairs of motors needs to be reversed
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+        // Wait for the drive to press the Start button on the Driver Hub
+        waitForStart();
+
+        // Loop until the robot is stopped
+        while (opModeIsActive()) {
+            // Get the gamepad inputs
+            // The y axis is vertical and "backwards",
+            // so we negate it to make it positive when going forward
+            float deltaY = -gamepad1.left_stick_y;
+            float deltaX = gamepad1.left_stick_x;
+
+            float left = deltaY + deltaX;
+            float right = deltaY - deltaX;
+
+            // Set the motor power
+            leftFront.setPower(left);
+            rightFront.setPower(right);
+            leftBack.setPower(left);
+            rightBack.setPower(right);
+        }
+    }
+}
